043d891e5164f0c886d55da20f272a7f0c0bdcc4
69bin <15611202192@126.com>
2024-03-25 22:42:23 +0800

    优化界面

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,24 +8,11 @@ use std::env;
 use std::io::Write;
 use std::{fs::File, io::Read};
 use tokio::io::AsyncReadExt;
-use once_cell::sync::Lazy;
-
-
-// 定义一个全局参数
-static SAVE_PATH: Lazy<String> = Lazy::new(|| {
-    // 这里是初始化全局参数的代码
-    "E:/rust/saveImage".to_string()
-});
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, open_image, matting_image])
+        .invoke_handler(tauri::generate_handler![open_image, matting_image])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -50,28 +37,7 @@ async fn open_image() -> Result<ImageParam, String> {
 
     let image_path = file.unwrap().path().to_str().unwrap().to_string();
     //读取图片转换为base64
-    let mut file = match File::open(&image_path) {
-        Ok(file) => file,
-        Err(err) => {
-            return Err(format!("{:?}", err));
-        }
-    };
-
-    let mut buffer = Vec::new();
-    match file.read_to_end(&mut buffer) {
-        Ok(_) => {
-            let base64_image = base64::encode(&buffer);
-            let mime_type = "image/jpeg";
-            let base64_with_prefix = format!("data:{};base64,{}", mime_type, base64_image);
-            return Ok(ImageParam {
-                image_base64: base64_with_prefix,
-                image_path: image_path,
-            });
-        }
-        Err(err) => {
-            return Err(format!("{:?}", err));
-        }
-    }
+    return image_to_base64(image_path);
 }
 
 /** 抠图 */
@@ -96,7 +62,7 @@ async fn matting_image(file_path: String) -> Result<ImageParam, String> {
     let client = reqwest::Client::new();
     let response = client
         .post("https://api.remove.bg/v1.0/removebg")
-        .header("X-Api-Key", "3Les12eNfx4tDkvHXnKc1h4d")
+        .header("X-Api-Key", "你创建的密钥")
         .multipart(form)
         .send()
         .await;
@@ -106,34 +72,13 @@ async fn matting_image(file_path: String) -> Result<ImageParam, String> {
             // 从响应中获取图片数据
             let image_data = res.bytes().await.unwrap();
             //获取项目目录
-            let save_path = "E:/rust/saveImage/image.jpg".to_string();
+            let save_path = "F:/images/image.jpg".to_string();
             println!("save:{:?}",save_path);
             // 将图片数据保存到文件
             let mut file = std::fs::File::create(&save_path).unwrap();
             let _ = file.write_all(&image_data);
             println!("保存成功!");
-            let mut file = match File::open(&save_path) {
-                Ok(file) => file,
-                Err(err) => {
-                    return Err(format!("{:?}", err));
-                }
-            };
-            let mut buffer = Vec::new();
-            match file.read_to_end(&mut buffer) {
-                Ok(_) => {
-                    let base64_image = base64::encode(&buffer);
-                    let mime_type = "image/jpeg";
-                    let base64_with_prefix = format!("data:{};base64,{}", mime_type, base64_image);
-                    return Ok(ImageParam {
-                        image_base64: base64_with_prefix,
-                        image_path: save_path,
-                    });
-                }
-                Err(err) => {
-                    println!("err = {:?}",err);
-                    return Err(format!("{:?}", err));
-                }
-            }
+            return image_to_base64(save_path);
         }
         Err(err) => {
             println!("{:?}", err);
@@ -143,3 +88,33 @@ async fn matting_image(file_path: String) -> Result<ImageParam, String> {
     Err("err".to_string())
 }
 
+
+
+/** 图片转换对象，包含base64 和 图片根路径 */
+fn image_to_base64(save_path: String) -> Result<ImageParam, String> {
+    // 打开文件
+    let mut file = match File::open(&save_path) {
+        Ok(file) => file,
+        Err(err) => {
+            return Err(format!("{:?}", err));
+        }
+    };
+    // 存放读取文件
+    let mut buffer = Vec::new();
+    match file.read_to_end(&mut buffer) {
+        Ok(_) => {
+            // 读取的内容转换成base64
+            let base64_image = base64::encode(&buffer);
+            let mime_type = "image/jpeg";
+            let base64_with_prefix = format!("data:{};base64,{}", mime_type, base64_image);
+            return Ok(ImageParam {
+                image_base64: base64_with_prefix,
+                image_path: save_path,
+            });
+        }
+        Err(err) => {
+            println!("err = {:?}",err);
+            return Err(format!("{:?}", err));
+        }
+    }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,3 @@
-
-
 <template>
   <div>
     <div style="width: 100%;">
@@ -9,36 +7,39 @@
       <el-image :fit="image_fit" style="width: 300px; height: 300px;margin: 40px;" :src="image_base64">
         <template #error>
           <div class="image-slot">
-            <div style="width: 300px;height: 300px;align-items: center;line-height: 300px;text-align: center;">暂无图片</div>
+            <div style="width: 300px;height: 300px;align-items: center;line-height: 300px;text-align: center;">暂无图片
+            </div>
           </div>
         </template>
       </el-image>
-      <el-image :fit="image_fit" style="width: 300px; height: 300px;margin: 40px;" :src="matting_base64">
+      <el-image v-loading="loading" :fit="image_fit" style="width: 300px; height: 300px;margin: 40px;"
+        :src="matting_base64">
         <template #error>
           <div class="image-slot">
-            <div style="width: 300px;height: 300px;align-items: center;line-height: 300px;text-align: center;">暂无图片</div>
+            <div style="width: 300px;height: 300px;align-items: center;line-height: 300px;text-align: center;">暂无图片
+            </div>
           </div>
         </template>
       </el-image>
     </div>
     <div style="width: 100%;text-align: center;">
-      <el-button style="width: 120px;" @click="matting" type="error">抠图</el-button>
-      <el-button style="width: 120px;" type="primary">保存</el-button>
+      <el-button :loading="loading" style="width: 120px;" @click="matting" type="error">抠图</el-button>
     </div>
   </div>
 </template>
 
 <script>
 import { invoke } from '@tauri-apps/api/tauri'
-
+import { ElMessage } from 'element-plus'
 export default {
   data() {
     return {
-      image_base64:"",
-      image_path:"",
-      matting_base64:"",
-      matting_path:"",
-      image_fit:"contain"
+      loading: false,
+      image_base64: "",
+      image_path: "",
+      matting_base64: "",
+      matting_path: "",
+      image_fit: "contain"
     }
   },
   methods: {
@@ -52,11 +53,22 @@ export default {
     },
     matting() {
       let that = this;
+      that.loading = true;
       console.log(this.image_path);
-      invoke('matting_image',{filePath:this.image_path}).then(res => {
+      invoke('matting_image', { filePath: this.image_path }).then(res => {
         console.log(res);
+        ElMessage({
+          type: 'success',
+          showClose: true,
+          message: "处理完成",
+          center: true,
+        })
         that.matting_base64 = res.image_base64;
         that.matting_path = res.image_path;
+        that.loading = false;
+      }, err => {
+        ElMessage.error(err)
+        that.loading = false;
       })
     }
   }
